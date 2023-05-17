51d6a10ba85d286f6e30ca888337e439db45ca40
NGO DINH DAI <ngodinhdai77@gmail.com>
2023-05-17 17:23:05 +0700

    commit

---
+++ src/components/Route/Hero/Hero.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../../../styles/styles";
+
+const Hero = () => {
+  return (
+    <div
+      className={`relative min-h-[70vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.noramlFlex}`}
+      style={{
+        backgroundImage:
+          "url(https://themes.rslahmed.dev/rafcart/assets/images/banner-2.jpg)",
+      }}
+    >
+      <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
+        <h1
+          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
+        >
+          Shop smart, shop easy <br /> all at your fingertips <br /> with our
+          ecommerce store.
+        </h1>
+        <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
+          Shop with us for all your needs, from gadgets to fashion, all in one
+          convenient place. <br /> Quality products, affordable prices, and fast
+          shipping. Shop now for a hassle-free experience.
+        </p>
+        <Link to="/products" className="inline-block">
+          <div className={`${styles.button} mt-5`}>
+            <span className="text-[#fff] font-[Poppins] text-[18px]">
+              Shop Now
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
